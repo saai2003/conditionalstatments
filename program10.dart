b3e44861e77d2f024611e43b2a44e65eaa31dffb
saai2003 <sainandan2003@gmail.com>
2024-07-19 14:25:58 +0530

    conditionalstatementson 19/7/2024

---
+++ program10.dart
@@ -0,0 +1,20 @@
+void main(){
+String weather="cloudy";
+
+switch(weather){
+case "sunny":
+break;
+
+case "snowy":
+print("its snow time");
+break;
+
+case "cloudy":
+print("take umbrella");
+break;
+
+default:print("invalid");
+break;
+
+}
+}
